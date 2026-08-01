712daaaeaebc47adc92e24e7eb2c15ca68b03485
Raju Racha <raju9420raju@gmail.com>
2026-08-01 20:51:52 +0530

    minor changes

---
+++ JavaBasics.java
@@ -0,0 +1,14 @@
+public class JavaBasics{
+    public static void main(String[] args) {
+
+       int A = 104;
+       int B = 100;
+       System.out.println("Add = " + (A+B));
+        System.out.println("Sub = " + (A-B));
+         System.out.println("Mul = " + (A*B));
+         System.out.println("Add = " + (A/B));
+           System.out.println("Add = " + (A%B));
+
+
+    }
+}
