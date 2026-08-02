5690d9d05ed47636ab493319c659d4b12d617a7b
Raju Racha <raju9420raju@gmail.com>
2026-08-02 15:20:18 +0530

    while loop

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,14 +1,35 @@
-public class JavaBasics{
+
+import java.util.*;
+public class JavaBasics {
     public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+     System.out.println("Enter value a : ");
+    int a = sc.nextInt();
+    System.out.println("Enter value b : ");
+    int b = sc.nextInt();
+    System.out.println("Enter value char : ");
+    char operator = sc.next().charAt(0);
+   
 
-       int A = 104;
-       int B = 100;
-       System.out.println("Add = " + (A+B));
-        System.out.println("Sub = " + (A-B));
-         System.out.println("Mul = " + (A*B));
-         System.out.println("Add = " + (A/B));
-           System.out.println("Add = " + (A%B));
 
+     switch (operator) {
+        case '+': System.out.println(a+b);
+             break;
 
-    }
+         case '-': System.out.println(a-b);   
+             break;
+
+         case '*': System.out.println(a*b);
+             break;
+
+      case '/': System.out.println(a/b);
+            break;
+
+     case '%': System.out.println(a%b);
+             break;
+
+     }
+}
+
 }
