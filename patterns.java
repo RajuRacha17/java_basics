bd34381a284bd703305442597540f02b1b95563d
Raju Racha <raju9420raju@gmail.com>
2026-08-04 00:43:24 +0530

    done inverted star pattern

--- patterns.java
+++ patterns.java
@@ -1,18 +1,16 @@
 public class patterns {
     public static void main(String[] args) {
-        int i = 1;
-        int j= 1;
-        int n=4;
-        int m=5;
+        
 
-
-        //Outer loop 
-        for (i=1; i<=n; i++) {
-            //Inner Loop
-            for (j=1; j<=m; j++) {
+    //inverted star Pattern
+        //outer loop.....
+        for (int i=1; i<=4; i++) {
+            //inner loop......
+            for (int j=1; j<=i; j++) {
                 System.out.print(" * ");
             }
-            System.out.println();
+
+        System.out.println();
         }
         
     }
