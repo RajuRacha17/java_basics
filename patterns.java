c1d921c0e1e257f0f6739bb80027436091c39350
Raju Racha <raju9420raju@gmail.com>
2026-08-03 22:24:08 +0530

    initial commit

---
+++ patterns.java
@@ -0,0 +1,15 @@
+public class patterns {
+    public static void main(String[] args) {
+        int i = 1;
+        int j= 1;
+
+        for(i=1; i<=4;i++) { 
+            System.out.print("*");
+            for(j=1; j<=5;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
