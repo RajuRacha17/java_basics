e3fdb6e8760e517f5d6c62428cdc0bcbc4c064f1
Raju Racha <raju9420raju@gmail.com>
2026-08-03 13:16:05 +0530

    Completed all loops

--- DoWhileLoop.java
+++ DoWhileLoop.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class DoWhileLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-       
-  
+        System.out.println("Enter your Number");
+        int n = sc.nextInt();
 
-        do { 
-            System.out.println("Enter a Number : ");
-             int n = sc.nextInt();
-             if (n%10 == 0) {
-                break;
-             }
-            System.out.println(n);
-           
-        } while (true);
+        if (n==2) {
+            System.out.println(n + " is prime number");
+        } else{
+            boolean isPrime = true;
+            for(int i = 2; i<=n-1;i++) {
+                if (n%2==0) {
+                  isPrime = false;
+                }
+         
+            }
+
+            if (isPrime == true) {
+                System.out.println(" is a Prime Number ");
+            } else {
+                 System.out.println(" Is not a prime number ");
+            }
+        }     
     }
 }
