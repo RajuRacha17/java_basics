fc1947f8d92cd3b5c0cd5637883d08bf4d7bd89d
Raju Racha <raju9420raju@gmail.com>
2026-08-04 14:55:36 +0530

    number pattern

--- patterns.java
+++ patterns.java
@@ -1,15 +1,15 @@
 public class patterns {
     public static void main(String[] args) {
-        
+        int n = 4;
 
     //inverted star Pattern
         //outer loop.....
         for (int i=1; i<=4; i++) {
             //inner loop......
-            for (int j=1; j<=i; j++) {
-                System.out.print(" * ");
-            }
-
+            for (int j=1; j<=i; j++) {     // printed the numbers   1
+                System.out.print( j );                         //   1 2
+            }                                                    // 1 2 3
+                                                                //  1 2 3 4
         System.out.println();
         }
         
