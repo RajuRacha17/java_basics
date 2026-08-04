c2d152c5688e802439fe8a23521c2bd38fe737d8
Raju Racha <raju9420raju@gmail.com>
2026-08-04 15:19:26 +0530

    printed characters pattern

--- patterns.java
+++ patterns.java
@@ -1,15 +1,17 @@
 public class patterns {
     public static void main(String[] args) {
+        
+        char ch = 'A';
         int n = 4;
 
     //inverted star Pattern
         //outer loop.....
         for (int i=1; i<=4; i++) {
             //inner loop......
-            for (int j=1; j<=i; j++) {     // printed the numbers   1
-                System.out.print( j );                         //   1 2
-            }                                                    // 1 2 3
-                                                                //  1 2 3 4
+            for (int j=1; j<=i; j++) {     //  printed the Characters.... 
+                System.out.print( ch );  // by syso of ( ch ),  ch++;
+                ch++;                          
+            }                                                    
         System.out.println();
         }
         
