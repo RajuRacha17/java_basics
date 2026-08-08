97bb34bbd8f1c89bcd07bee83ecd867e62b00365
Raju Racha <raju9420raju@gmail.com>
2026-08-08 21:42:57 +0530

    Diamond pattern

--- AdvancedPattern.java
+++ AdvancedPattern.java
@@ -1,26 +1,39 @@
 public class AdvancedPattern {
        
-        public static void Hollow_Rhombus(int n ) {
+        public static void Diamond(int n ) {
           for (int i=1; i<=n; i++) {
             // spaces
             for (int j =1; j<=(n-i); j++) {
               System.out.print(" ");
             }
-
-            //hollow rectangle --stars
-            for (int j =1; j<=n ; j++) {
-              if (i == 1 || i == n || j == 1 || j ==n ) {
-                System.out.print("*");
-              }else {
-                System.out.print(" ");
-              }
+          
+            //stars
+            for (int j =1 ; j<=(2*i)-1; j++) {
+              System.out.print("*");
             }
+            
+            System.out.println();
+          }
+
+          // 2nd Half
+
+          for (int i=n; i>=1; i--) {
+            // spaces
+            for (int j =1; j<=(n-i); j++) {
+              System.out.print(" ");
+            }
+          
+            //stars
+            for (int j =1 ; j<=(2*i)-1; j++) {
+              System.out.print("*");
+            }
+            
             System.out.println();
           }
         }
     public static void main(String[] args) {
     
-     Hollow_Rhombus(8);
+     Diamond(7);
 
     }
 }
