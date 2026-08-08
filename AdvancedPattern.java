fef424c335cb429bcf173e110264f49977961098
Raju Racha <raju9420raju@gmail.com>
2026-08-08 21:04:46 +0530

    Solid Rhombus

--- AdvancedPattern.java
+++ AdvancedPattern.java
@@ -1,50 +1,23 @@
 public class AdvancedPattern {
-       public static void Butterfly(int n ) {
-        //1st half..
-        for (int i = 1; i<=n ; i++){
-          //stars i..
-          for (int j = 1; j<=i ; j++) {
-            System.out.print("*");
+       
+        public static void solid_Rhombus(int n ) {
+          for (int i=1; i<=n; i++) {
+            // spaces
+            for (int j =1; j<=(n-i); j++) {
+              System.out.print(" ");
+            }
+
+            //stars..
+            for(int j =1; j<=n; j++) {
+              System.out.print("*");
+            }
+            System.out.println();
+
           }
-
-          //spaces -2*(n-1)
-          for (int j =1 ; j<=2*(n-i); j++) {
-            System.out.print(" ");
-          }
-
-          //stars i
-          for (int j =1; j<=i; j++) {
-            System.out.print("*");
-          }
-
-          System.out.println();
-
         }
-        //2nd half
-
-        for (int i=n ; i>=1; i--) {
-           //stars i..
-          for (int j = 1; j<=i ; j++) {
-            System.out.print("*");
-          }
-
-          //spaces -2*(n-1)
-          for (int j =1 ; j<=2*(n-i); j++) {
-            System.out.print(" ");
-          }
-
-          //stars i
-          for (int j =1; j<=i; j++) {
-            System.out.print("*");
-          }
-          System.out.println();
-        }
-        //  System.out.println();
-       }
-    
     public static void main(String[] args) {
     
-     Butterfly(5);
+     solid_Rhombus(5);
 
     }
 }
