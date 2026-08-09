48ccbba7f14f0d06e0ecce0e78d937486bd1350e
Raju Racha <raju9420raju@gmail.com>
2026-08-10 00:51:32 +0530

    check Number prime or not

---
+++ Functions_Methods/Check_Prime_or_not.java
@@ -0,0 +1,19 @@
+package Functions_Methods;
+
+public class Check_Prime_or_not {
+    public static boolean isprime(int n) {
+        boolean isprime = true;
+        for (int i =2 ; i <= n-1 ; i++) {
+            if (n % i == 0) {
+                isprime = false;
+            }
+        }
+        return isprime;
+    }
+    
+    public static void main(String[] args) {
+    System.out.println(isprime(100));
+
+    }
+    
+}
