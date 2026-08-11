4556d9a4833968b66db3aa636dd10e5860e14be2
Raju Racha <raju9420raju@gmail.com>
2026-08-11 22:25:03 +0530

    Initial commit

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,10 +1,27 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
-        // int marks [] = new int [50];
-        String Fruits[] = {"Apple" , "Mango" , "Banana"};
-         
+        
+        int marks [] = new int [100];
+        System.out.println("Length of Array is " + marks.length);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Marks of Phy");
+        marks[0] = sc.nextInt();
+        System.out.println("Enter Marks of che");
+        marks[1] = sc.nextInt();
+        System.out.println("Enter Marks of maths");
+        marks[2] = sc.nextInt();
+
+        System.out.println("Physics = " + marks[0]);
+         System.out.println("Chemistry = " + marks[1]);
+         System.out.println("Maths = " + marks[2]);
+
+         int percentage = (marks[0] + marks[1] + marks[2])  / 3;
+         System.out.println("percentage is = " + percentage + "%");
 
     }
 }
