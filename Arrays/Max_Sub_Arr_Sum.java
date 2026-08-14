9a86a9c70cc602b7df460503459c3a22f4ab5919
Raju Racha <raju9420raju@gmail.com>
2026-08-15 00:39:57 +0530

    Code Updated

--- Arrays/Max_Sub_Arr_Sum.java
+++ Arrays/Max_Sub_Arr_Sum.java
@@ -1,32 +1,36 @@
 package Arrays;
 
 public class Max_Sub_Arr_Sum {
-    public static void max_sub_arrrSum(int numbers [] ) {
-        int currSum =0;
-        int MaxSum =Integer.MAX_VALUE;
+    public static void MaxSubArray(int numbers[]) {
+
+        int currSum = 0;
+        int MaxSum = Integer.MIN_VALUE;
+
 
         for (int i = 0; i<numbers.length; i++) {
             int start = i;
-            for (int j =i ; i<numbers.length; i++) {
+
+            for (int j = i; j<numbers.length;j++){
                 int end = j;
-                currSum =0;
-                for(int k = start; k<= end;k++) {
+                currSum = 0;
+
+                for (int k=start; k<=end; k++) {
                     currSum += numbers[k];
                 }
+                
                 System.out.println(currSum);
-                if ( MaxSum < currSum) {
+                if(MaxSum < currSum){
                     MaxSum = currSum;
                 }
             }
-
+            System.out.println();
         }
-        System.out.println("Max Sum is = " + MaxSum);
+        System.out.println("The Max Sub Arrays Are = " + MaxSum);
     }
     public static void main(String[] args) {
         int numbers[] = {2,4,6,8,10};
 
-        max_sub_arrrSum(numbers);
-        
+        MaxSubArray(numbers);
 
     }
 }
