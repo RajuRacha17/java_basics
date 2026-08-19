5142aff55f44fa17c7fb780481ee3e315ef5c560
Raju Racha <raju9420raju@gmail.com>
2026-08-19 22:42:45 +0530

    Add comments for clarity in product.java
    Added comments to clarify the purpose of the multiply method and variable initialization.

--- Functions_Methods/product.java
+++ Functions_Methods/product.java
@@ -3,11 +3,13 @@ package Functions_Methods;
 public class product {
 
     public static int multiply (int a , int b ){
+        // multiplying a and b stored in Variable product 
         int product = a*b;
         return product;
     }
 
     public static void main(String[] args) {
+        // initialising variables and storing the values 
         int a = 5;
         int b = 4;
         int prod = multiply(a, b);
