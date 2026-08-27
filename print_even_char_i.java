ede8345c15b13109629799db800a2481c9a60a63
Raju Racha <raju9420raju@gmail.com>
2026-08-27 20:04:38 +0530

    print the even postioned characters of given string

---
+++ print_even_char_i.java
@@ -0,0 +1,14 @@
+public class print_even_char_i {
+    
+    public static void main(String[] args) {
+
+        String s = "GeeksForGeeks";
+
+         for (int i = 0 ; i<s.length(); i++) {
+            // char ch = s.charAt(i);
+            if ( i % 2 == 0 ) {
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
