22415f57cedd5b5a270d29b676ee0ea544214aa4
Raju Racha <raju9420raju@gmail.com>
2026-08-29 22:11:24 +0530

    Bitwise Shift Operators

---
+++ Bitwise_shift_operators.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bitwise_shift_operators {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // code here
+         System.out.println((a >> b) + " " + (a << b));
+         sc.close();
+    }
+}
