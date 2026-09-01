ffa68c7dc6d3521da2ddef48a83905a77abe8675
Raju Racha <raju9420raju@gmail.com>
2026-09-01 21:48:17 +0530

    Printing any given string to nth Times

---
+++ Print_gfg_nthTime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Print_gfg_nthTime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        for(int i = 1; i<=n; i++) {
+            System.out.print("GFG ");
+            sc.close();
+        }
+    }
+}
