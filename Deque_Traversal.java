30b305af304cd70d6a1bcd6382ddcb843a4ebeb6
Raju Racha <raju9420raju@gmail.com>
2026-09-02 19:21:30 +0530

    Deque Traversal

---
+++ Deque_Traversal.java
@@ -0,0 +1,9 @@
+public class Deque_Traversal {
+    public static void main(String[] args) {
+     int [] deq = { 1, 2, 3, 4, 5};
+        for(int i : deq){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
