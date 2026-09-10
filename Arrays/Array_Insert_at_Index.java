35dfeba109e9c5259f0428ee52eab4e8a91856ed
Raju Racha <raju9420raju@gmail.com>
2026-09-10 22:23:11 +0530

    in the array inserting an element in the given index i.e val and idx

---
+++ Arrays/Array_Insert_at_Index.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class Array_Insert_at_Index {
+     public void insertAtIndex(ArrayList<Integer> arr, int index, int val) {
+        // code here
+        arr.add(index,val);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        Array_Insert_at_Index inserter = new Array_Insert_at_Index();
+        inserter.insertAtIndex(list, 2, 10);
+
+        System.out.println(list);
+    }
+}
